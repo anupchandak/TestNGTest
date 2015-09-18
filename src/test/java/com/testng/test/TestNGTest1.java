0ecb64ac968a3d6f24b85e09bb034ea2def7e1ce
anupchandak <anupchandak@gmail.com>
2015-09-18 16:39:08 +0530

    For TestNG issue#800
    For TestNG issue#800

---
+++ src/test/java/com/testng/test/TestNGTest1.java
@@ -0,0 +1,22 @@
+package com.testng.test;
+
+
+
+import org.testng.annotations.Test;
+
+/**
+ * Test class
+ * 
+ * @author Anup Chandak
+ */
+public class TestNGTest1 {
+
+	/**
+	 * Method.
+	 */
+	@Test
+	public void testForTestNGAttributeTest1() {
+		System.out.println("I am in Test method of class: TestNGTest1");
+	}
+
+}
---
+++ src/test/java/com/testng/test/TestNGTest2.java
@@ -0,0 +1,22 @@
+package com.testng.test;
+
+
+
+import org.testng.annotations.Test;
+
+/**
+ * Test class
+ * 
+ * @author Anup Chandak
+ */
+public class TestNGTest2 {
+
+	/**
+	 * Method
+	 */
+	@Test
+	public void testForTestNGAttributeTest2() {
+		System.out.println("I am in Test method of class: TestNGTest2");
+	}
+
+}
